3a8cd4e0453fd84a9f83f83e09afafcf6b79ebc4
mahendra-918 <kasulamahi624@gmail.com>
2026-01-09 20:21:30 +0530

    data transformation done

--- main.py
+++ main.py
@@ -2,23 +2,32 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
 import sys
+from networksecurity.components.data_transformation import DataTransformation
 
 if __name__ == "__main__":
     try:
-        traningpipelineconfig = TrainingPipelineConfig()
-        dataingestionconfig = DataIngestionConfig(traningpipelineconfig)
-        data_ingestion = DataIngestion(dataingestionconfig)
+        traning_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(traning_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
         logging.info("initiated the data ingestion")
-        dataingestionartifact = data_ingestion.initiate_data_ingestion()
-        print(dataingestionartifact)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        print(data_ingestion_artifact)
         logging.info("Data initiation completed")
-        datavalidationconfig = DataValidationConfig(traningpipelineconfig)
-        data_validation = DataValidation(dataingestionartifact,datavalidationconfig)
+        data_validation_config = DataValidationConfig(traning_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact,data_validation_config)
         logging.info("Initiate the data validation")
-        datavalidationartifact = data_validation.initiate_data_validation()
+        data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("data validation is completed")
+        print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(traning_pipeline_config)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
+
         
         
     except Exception as e:
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-
+import os
+from networksecurity.constants import training_pipeline
 @dataclass
 class DataIngestionArtifact:
     trained_file_path:str
@@ -13,3 +14,9 @@ class DataValidationArtifact:
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
